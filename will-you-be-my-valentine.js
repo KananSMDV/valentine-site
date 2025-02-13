d18432697c4609be6f5f4a5c46b9573d04f97719
KananSMDV <124068875+KananSMDV@users.noreply.github.com>
2025-02-13 22:08:44 +0400

    Add files via upload

---
+++ will-you-be-my-valentine.js
@@ -0,0 +1,68 @@
+let messages = ["Эх... Может, передумаешь?🥺", "Ты разбиваешь мне сердце...💔", "Система в шоке…😱", "Ошибка 404: кнопка 'Нет' скоро пропадёт!🤭", "Ты точно понимаешь, что делаешь?!😭", "Хорошо, ты победила. Ахах, нет, не победила!😆"];
+let noCount = 0;
+let noButton = document.getElementById("no");
+let yesButton = document.getElementById("yes");
+let messageText = document.getElementById("message");
+
+noButton.addEventListener("click", rejectLove);
+yesButton.addEventListener("click", acceptLove);
+
+function rejectLove() {
+    if (noCount < messages.length) {
+        messageText.innerText = messages[noCount];
+        noCount++;
+        noButton.style.transform = `scale(${1 - noCount * 0.1})`;
+        yesButton.style.transform = `scale(${1 + noCount * 0.1})`;
+    }
+    if (noCount === messages.length) {
+        noButton.style.display = "none";
+    }
+}
+
+function acceptLove() {
+    document.getElementById("valentine").innerHTML = `
+        <img src="https://media1.tenor.com/m/aEWN44So2ckAAAAC/kiss-kisses.gif" class="gif">
+        <div class="question">❤️Ты моя самая лучшая Валентинка!❤️</div>
+    `;
+    launchConfetti();
+    startHeartRain();
+}
+
+function launchConfetti() {
+    var duration = 3 * 1000;
+    var end = Date.now() + duration;
+
+    (function frame() {
+        confetti({
+            particleCount: 5,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 }
+        });
+        confetti({
+            particleCount: 5,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 }
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    })();
+}
+
+function startHeartRain() {
+    const heartContainer = document.getElementById("heart-container");
+    setInterval(() => {
+        let heart = document.createElement("div");
+        heart.classList.add("heart");
+        heart.innerHTML = "❤️";
+        heart.style.left = Math.random() * window.innerWidth + "px";
+        heartContainer.appendChild(heart);
+
+        setTimeout(() => {
+            heart.remove();
+        }, 5000);
+    }, 300);
+}
